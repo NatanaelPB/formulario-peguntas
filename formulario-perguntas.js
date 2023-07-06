be7d85caad417749c19374e2d4e4e29274055e79
NatanaelPB <natanaelcorrea2010@gmail.com>
2023-07-06 20:48:12 -0300

    adicionando projetos

---
+++ formulario-perguntas.js
@@ -0,0 +1,25 @@
+  const nome = "Natanael C Silva"; 
+  const idade = 44; 
+  const linguagem = "JavaScript"; 
+  const mensagem = `Olá ${Natanael C Silva}, você tem ${44} anos e já está aprendendo ${Javascript}!`;
+
+  alert.(msg);
+
+  const gosta = prompt('Você gosta de estudar [linguagem]? Responda com o número 1 para SIM ou 2 para NÃO');
+  if (gosta == 1){
+    alert ('Muito bom! Continue estudando e você terá muito sucesso.');
+  } 
+  
+  if (gosta == 2) {
+      alert ('Ahh que pena... Já tentou aprender outras linguagens?');
+  } 
+   
+
+
+
+
+
+
+
+
+
